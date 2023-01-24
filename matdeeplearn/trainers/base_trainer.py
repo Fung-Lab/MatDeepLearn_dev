600756548f8fc19286c7db9a0b9512ca94543ff5
Sarah Eisenach <sarah@eisenach.org>
2023-01-24 12:01:14 -0500

    fixing config for wandb

--- matdeeplearn/trainers/base_trainer.py
+++ matdeeplearn/trainers/base_trainer.py
@@ -113,28 +113,30 @@ class BaseTrainer(ABC):
         identifier = config["task"].get("identifier", None)
         verbosity = config["task"].get("verbosity", None)
 
+        # wandb_config = {
+        #     "model_params": config["model"],
+        #     "optimizer_hyperparams": {
+        #         "start_lr": config["optim"]["lr"],
+        #         "epochs": max_epochs,
+        #         "batch_size": config["optim"]["batch_size"],
+        #         "scheduler_args": config["optim"]["scheduler"]["scheduler_args"],
+        #     },
+        #     # "preprocess_params": config["optim"]["preprocess_params"],
+        #     "splits": {
+        #         "train": config["dataset"]["train_ratio"],
+        #         "val": config["dataset"]["val_ratio"],
+        #         "test": config["dataset"]["test_ratio"],
+        #     },
+        # }
+
         wandb.init(
             settings=wandb.Settings(start_method="fork"),
             project="DOS_cgcnn",
             entity="fung-lab",
             resume="allow",
             name=identifier,
+            config=config,
         )
-        wandb.config = {
-            # "model_hyperparams": self.model_config["hyperparams"],
-            "optimizer_hyperparams": {
-                "lr": config["optim"]["lr"],
-                "epochs": max_epochs,
-                "batch_size": config["optim"]["batch_size"],
-                "scheduler_args": config["optim"]["scheduler"]["scheduler_args"],
-            },
-            # "preprocess_params": config["optim"]["preprocess_params"],
-            "splits": {
-                "train": config["dataset"]["train_ratio"],
-                "val": config["dataset"]["val_ratio"],
-                "test": config["dataset"]["test_ratio"],
-            },
-        }
 
         return cls(
             model=model,
