866bb0726951bb54e2cf172760892e490b5003b3
Rithwik Seth <rithwikseth@gmail.com>
2023-02-22 19:21:26 -0500

    distanceCalc

--- matdeeplearn/models/utils.py
+++ matdeeplearn/models/utils.py
@@ -381,6 +381,7 @@ def radius_graph_pbc(
         unit_cell = unit_cell.view(-1, 3)
 
     edge_index = torch.stack((index2, index1))
+    print(edge_index.size())
     edge_index = edge_index[:,edge_index[0] != edge_index[1]]
 
     return edge_index, unit_cell, num_neighbors_image
