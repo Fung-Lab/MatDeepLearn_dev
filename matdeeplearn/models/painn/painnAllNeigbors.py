5a9f004601b1acf4a22eb1c20a4bacea83175867
Rithwik Seth <rithwikseth@gmail.com>
2023-02-25 13:31:34 -0500

    Allneighbors

--- matdeeplearn/models/painn/painnAllNeigbors.py
+++ matdeeplearn/models/painn/painnAllNeigbors.py
@@ -349,13 +349,13 @@ class PaiNN(BaseModel):
         if self.regress_forces and not self.direct_forces:
             pos = pos.requires_grad_(True)
 
-        (
-            edge_index,
-            neighbors,
-            edge_dist,
-            edge_vector,
-            id_swap,
-        ) = self.generate_graph_values(data)
+        #(
+        #    edge_index,
+        #    neighbors,
+        #    edge_dist,
+        #    edge_vector,
+        #    id_swap,
+        #) = self.generate_graph_values(data)
 
         edge_index = data.edge_index
         sorted_indices = torch.argsort(edge_index[1])
