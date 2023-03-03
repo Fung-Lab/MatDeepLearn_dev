a1d753f5e1a19e0fe12580e768d882d4bde72794
Rithwik Seth <rithwikseth@gmail.com>
2023-03-03 17:46:30 -0500

    Allneighbors

--- matdeeplearn/preprocessor/processorNew.py
+++ matdeeplearn/preprocessor/processorNew.py
@@ -5,11 +5,10 @@ import os
 import numpy as np
 import pandas as pd
 import torch
-import ase
-from ase import io, Atoms, neighborlist
+import wandb
+from ase import io
 from torch_geometric.data import Data, InMemoryDataset
 from torch_geometric.transforms import Compose
-from torch_geometric.utils import dense_to_sparse
 from tqdm import tqdm
 
 from matdeeplearn.common.registry import registry
@@ -17,7 +16,8 @@ from matdeeplearn.preprocessor.helpers import (
     clean_up,
     generate_edge_features,
     generate_node_features,
-    get_cutoff_distance_matrix,
+    generate_virtual_nodes,
+    calculate_edges_master,
 )
 
 
@@ -25,9 +25,10 @@ def process_data(dataset_config):
     root_path = dataset_config["src"]
     target_path = dataset_config["target_path"]
     pt_path = dataset_config.get("pt_path", None)
-    cutoff_radius = dataset_config["cutoff_radius"]
-    n_neighbors = dataset_config["n_neighbors"]
-    edge_steps = dataset_config["edge_steps"]
+    cutoff_radius = dataset_config["preprocess_params"]["cutoff_radius"]
+    n_neighbors = dataset_config["preprocess_params"]["n_neighbors"]
+    num_offsets = dataset_config["preprocess_params"]["num_offsets"]
+    edge_steps = dataset_config["preprocess_params"]["edge_steps"]
     data_format = dataset_config.get("data_format", "json")
     image_selfloop = dataset_config.get("image_selfloop", True)
     self_loop = dataset_config.get("self_loop", True)
@@ -35,6 +36,8 @@ def process_data(dataset_config):
     additional_attributes = dataset_config.get("additional_attributes", [])
     verbose: bool = dataset_config.get("verbose", True)
     all_neighbors = dataset_config["all_neighbors"]
+    edge_calc_method = dataset_config.get("edge_calc_method", "mdl")
+    use_wandb = dataset_config.get("use_sweep_params", False)
     device: str = dataset_config.get("device", "cpu")
 
     processor = DataProcessor(
@@ -43,6 +46,7 @@ def process_data(dataset_config):
         pt_path=pt_path,
         r=cutoff_radius,
         n_neighbors=n_neighbors,
+        num_offsets=num_offsets,
         edge_steps=edge_steps,
         transforms=dataset_config.get("transforms", []),
         data_format=data_format,
@@ -52,8 +56,11 @@ def process_data(dataset_config):
         additional_attributes=additional_attributes,
         verbose=verbose,
         all_neighbors=all_neighbors,
+        edge_calc_method=edge_calc_method,
+        use_wandb=use_wandb,
         device=device,
     )
+
     processor.process()
 
 
@@ -65,6 +72,7 @@ class DataProcessor:
         pt_path: str,
         r: float,
         n_neighbors: int,
+        num_offsets: int,
         edge_steps: int,
         transforms: list = [],
         data_format: str = "json",
@@ -74,56 +82,46 @@ class DataProcessor:
         additional_attributes: list = [],
         verbose: bool = True,
         all_neighbors: bool = False,
+        edge_calc_method: str = "mdl",
+        use_wandb: bool = False,
         device: str = "cpu",
     ) -> None:
         """
         create a DataProcessor that processes the raw data and save into data.pt file.
-
         Parameters
         ----------
             root_path: str
                 a path to the root folder for all data
-
             target_file_path: str
                 a path to a CSV file containing target y values
-
             pt_path: str
                 a path to the directory to which data.pt should be saved
-
             r: float
                 cutoff radius
-
             n_neighbors: int
                 max number of neighbors to be considered
                 => closest n neighbors will be kept
-
             edge_steps: int
                 step size for creating Gaussian basis for edges
                 used in torch.linspace
-
+            otf: bool
+                default False. Whether or not to transform the data on the fly.
             transforms: list
                 default []. List of transforms to apply to the data.
-
             data_format: str
                 format of the raw data file
-
             image_selfloop: bool
                 if True, add self loop to node and set the distance to
                 the distance between node and its closest image
-
             self_loop: bool
                 if True, every node in a graph will have a self loop
-
             node_representation: str
                 a path to a JSON file containing vectorized representations
                 of elements of atomic numbers from 1 to 100 inclusive.
-
                 default: one-hot representation
-
             additional_attributes: list of str
                 additional user-specified attributes to be included in
                 a Data() object
-
             verbose: bool
                 if True, certain messages will be printed
         """
@@ -133,6 +131,7 @@ class DataProcessor:
         self.pt_path = pt_path
         self.r = r
         self.n_neighbors = n_neighbors
+        self.num_offsets = num_offsets
         self.edge_steps = edge_steps
         self.data_format = data_format
         self.image_selfloop = image_selfloop
@@ -141,14 +140,18 @@ class DataProcessor:
         self.additional_attributes = additional_attributes
         self.verbose = verbose
         self.all_neighbors = all_neighbors
+        self.edge_calc_method = edge_calc_method
+        self.use_wandb = use_wandb
         self.device = device
         self.transforms = transforms
         self.disable_tqdm = logging.root.level > logging.INFO
 
     def src_check(self):
         if self.target_file_path:
+            logging.debug("ASE wrap")
             return self.ase_wrap()
         else:
+            logging.debug("JSON wrap")
             return self.json_wrap()
 
     def ase_wrap(self):
@@ -164,7 +167,9 @@ class DataProcessor:
         dict_structures = []
         ase_structures = []
 
-        logging.info("Converting data to standardized form for downstream processing.")
+        logging.info(
+            "(ASE) Converting data to standardized form for downstream processing."
+        )
         for i, structure_id in enumerate(file_names):
             p = os.path.join(self.root_path, str(structure_id) + "." + self.data_format)
             ase_structures.append(io.read(p))
@@ -174,7 +179,7 @@ class DataProcessor:
             pos = torch.tensor(s.get_positions(), device=self.device, dtype=torch.float)
             cell = torch.tensor(
                 np.array(s.get_cell()), device=self.device, dtype=torch.float
-            ).view(1, 3, 3)
+            )
             atomic_numbers = torch.LongTensor(s.get_atomic_numbers())
 
             d["positions"] = pos
@@ -229,15 +234,19 @@ class DataProcessor:
             else 1
         )
 
-        logging.info("Converting data to standardized form for downstream processing.")
+        logging.info(
+            "(JSON) Converting data to standardized form for downstream processing."
+        )
         for i, s in enumerate(tqdm(original_structures, disable=self.disable_tqdm)):
             d = {}
             pos = torch.tensor(s["positions"], device=self.device, dtype=torch.float)
             cell = torch.tensor(s["cell"], device=self.device, dtype=torch.float)
+            #cell2 = s["cell2"]
             atomic_numbers = torch.LongTensor(s["atomic_numbers"])
 
             d["positions"] = pos
             d["cell"] = cell
+            #d["cell2"] = cell2
             d["atomic_numbers"] = atomic_numbers
             d["structure_id"] = s["structure_id"]
 
@@ -284,76 +293,85 @@ class DataProcessor:
 
         logging.info("Getting torch_geometric.data.Data() objects.")
 
+        # find the virtual nodes transform (workaround for now)
+        transforms = [
+            (i, t)
+            for (i, t) in enumerate(
+                wandb.config.get("transforms") if self.use_wandb else self.transforms
+            )
+        ]
+        virtual_nodes_transform = None
+        for i, t in transforms:
+            if t.get("name") == "VirtualNodes":
+                virtual_nodes_transform = t
+                break
+
         for i, sdict in enumerate(tqdm(dict_structures, disable=self.disable_tqdm)):
             target_val = y[i]
             data = data_list[i]
 
             pos = sdict["positions"]
             cell = sdict["cell"]
+            #cell2 = sdict["cell2"]
             atomic_numbers = sdict["atomic_numbers"]
             structure_id = sdict["structure_id"]
-            if self.all_neighbors == False:
-                cd_matrix, cell_offsets, atom_rij = get_cutoff_distance_matrix(
-                    pos,
-                    cell,
-                    self.r,
-                    self.n_neighbors,
-                    image_selfloop=self.image_selfloop,
-                    device=self.device,
-                )
-                edge_indices, edge_weights = dense_to_sparse(cd_matrix)
-                if(atom_rij.dim() > 1):
-                  edge_vec = atom_rij[edge_indices[0], edge_indices[1]]
-            elif self.all_neighbors == True:
-                #cd_matrix, cell_offsets, _ = get_cutoff_distance_matrix(
-                #    pos,
-                #    cell,
-                #    self.r,
-                #    self.n_neighbors,
-                #    image_selfloop=self.image_selfloop,
-                #    device=self.device,
-                #)
-                #edge_indices, edge_weights = dense_to_sparse(cd_matrix)
-                
-                first_idex, second_idex, rij, rij_vec, shifts = ase.neighborlist.primitive_neighbor_list("ijdDS", (True,True,True), ase.geometry.complete_cell(cell.squeeze()), pos.numpy(), cutoff=self.r, self_interaction=False, use_scaled_positions=False)   
-                # Eliminate true self-edges that don't cross periodic boundaries (https://github.com/mir-group/nequip/blob/main/nequip/data/AtomicData.py)
-                bad_edge = first_idex == second_idex
-                bad_edge &= np.all(shifts == 0, axis=1)
-                keep_edge = ~bad_edge
-                first_idex = first_idex[keep_edge]
-                second_idex = second_idex[keep_edge]
-                rij = rij[keep_edge] 
-                rij_vec = rij_vec[keep_edge]
-                shifts = shifts[keep_edge]
-                # get closest n neighbors
-                if len(rij) > self.n_neighbors:
-                    _, topk_indices = torch.topk(torch.tensor(rij), self.n_neighbors, largest=False, sorted=False)
-                    first_idex = first_idex[topk_indices]
-                    second_idex = second_idex[topk_indices]
-                    rij = rij[topk_indices] 
-                    rij_vec = rij_vec[topk_indices]
-                    shifts = shifts[topk_indices]    
-                                  
-                edge_indices = torch.stack([torch.LongTensor(torch.tensor(first_idex)), torch.LongTensor(torch.tensor(second_idex))], dim=0)
-                edge_vec = torch.tensor(rij_vec).float()
-                edge_weights = torch.tensor(rij).float()   
-                cell_offsets = torch.tensor(shifts).int()                
-                
+
+            data.o_pos = pos.clone()
+            data.o_z = atomic_numbers.clone()
+
+            edge_gen_out = calculate_edges_master(
+                self.edge_calc_method,
+                False,
+                self.r,
+                self.n_neighbors,
+                self.num_offsets,
+                structure_id,
+                cell,
+                pos,
+                atomic_numbers,
+                remove_virtual_edges=False,
+                experimental_distance=False,
+                device=self.device,
+            )
+
+            edge_indices = edge_gen_out["edge_index"]
+            edge_weights = edge_gen_out["edge_weights"]
+            cell_offsets = edge_gen_out["cell_offsets"]
+            edge_vec = edge_gen_out["edge_vec"]
+
+            # virtual node generation (workaround for now)
+            #if virtual_nodes_transform:
+            #    vpos, virtual_z = generate_virtual_nodes(
+            #        cell2,
+            #        virtual_nodes_transform.get("virtual_box_increment", 3.0),
+            #        self.device,
+            #    )
+            #    pos = torch.cat([pos, vpos], dim=0)
+            #    atomic_numbers = torch.cat([atomic_numbers, virtual_z], dim=0)
+
             data.n_atoms = len(atomic_numbers)
             data.pos = pos
             data.cell = cell
+            #data.cell2 = cell2
             data.y = torch.Tensor(np.array([target_val]))
             data.z = atomic_numbers
             data.u = torch.Tensor(np.zeros((3))[np.newaxis, ...])
             data.edge_index, data.edge_weight = edge_indices, edge_weights
-            data.edge_vec = edge_vec
             data.cell_offsets = cell_offsets
 
             data.edge_descriptor = {}
-            # data.edge_descriptor["mask"] = cd_matrix_masked
             data.edge_descriptor["distance"] = edge_weights
             data.distances = edge_weights
+            data.edge_vec = edge_vec
             data.structure_id = [[structure_id] * len(data.y)]
+            print(data.n_atoms)
+            print(data.pos.size())
+            print(data.cell.size())
+            print(data.z.size())
+            print(data.edge_index.size())
+            print(data.edge_weight.size())
+            print(data.edge_vec.size())
+            print(data.cell_offsets.size())
 
             # add additional attributes
             if self.additional_attributes:
@@ -367,28 +385,26 @@ class DataProcessor:
         generate_edge_features(data_list, self.edge_steps, self.r, device=self.device)
 
         # compile non-otf transforms
-        logging.debug("Applying transforms.")
-
-        # Ensure GetY exists to prevent downstream model errors
-        assert "GetY" in [
-            tf["name"] for tf in self.transforms
-        ], "The target transform GetY is required in config."
-
+        logging.info("Applying transforms.")
         transforms_list = []
         for transform in self.transforms:
-            if not transform.get("otf", False):
+            # go through dict and overwrite with wandb config for sweep purposes
+            if self.use_wandb:
+                for key in transform["args"]:
+                    if key in wandb.config:
+                        transform["args"][key] = wandb.config[key]
+
+            if not transform["otf"]:
                 transforms_list.append(
                     registry.get_transform_class(
-                        transform["name"],
-                        **({} if transform["args"] is None else transform["args"])
+                        transform["name"], **transform.get("args", {})
                     )
                 )
-
         composition = Compose(transforms_list)
 
         # apply transforms
-        for data in data_list:
-            composition(data)
+        for i, data in enumerate(tqdm(data_list, disable=self.disable_tqdm)):
+            data_list[i] = composition(data)
 
         clean_up(data_list, ["edge_descriptor"])
 
