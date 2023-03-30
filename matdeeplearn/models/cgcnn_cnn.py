2d546a734724db290c9e9505d117fcf5c7cd18cd
Sarah Eisenach <sarah@eisenach.org>
2023-03-30 11:57:02 -0400

    updating cgcnn cnn model with spd data

--- matdeeplearn/models/cgcnn_cnn.py
+++ matdeeplearn/models/cgcnn_cnn.py
@@ -1,7 +1,16 @@
 import torch
-import wandb
-from torch.nn import AdaptiveAvgPool1d, BatchNorm1d, Dropout, Linear, ReLU, Sequential
+from torch.nn import (
+    AdaptiveAvgPool1d,
+    AdaptiveMaxPool1d,
+    BatchNorm1d,
+    Dropout,
+    Linear,
+    ReLU,
+    Sequential,
+    Sigmoid,
+)
 
+import wandb
 from matdeeplearn.common.registry import registry
 from matdeeplearn.models.cgcnn import CGCNN
 
@@ -46,39 +55,44 @@ class CGCNN_CNN(CGCNN):
 
         # set up CNN
         self.conv_layer1 = Sequential(
-            torch.nn.Conv1d(in_channels=1, out_channels=8, kernel_size=3),
+            torch.nn.Conv1d(in_channels=3, out_channels=16, kernel_size=5),
             ReLU(),
-            BatchNorm1d(8),
+            BatchNorm1d(16),
             AdaptiveAvgPool1d(100),
         )
         self.conv_layer2 = Sequential(
-            torch.nn.Conv1d(in_channels=8, out_channels=16, kernel_size=3),
+            torch.nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3),
             ReLU(),
+            BatchNorm1d(32),
             AdaptiveAvgPool1d(50),
         )
         self.conv_layer3 = Sequential(
-            torch.nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3),
+            torch.nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3),
             ReLU(),
+            BatchNorm1d(64),
             AdaptiveAvgPool1d(25),
         )
         self.conv_layer4 = Sequential(
-            torch.nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3),
+            torch.nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3),
             ReLU(),
+            BatchNorm1d(128),
             AdaptiveAvgPool1d(12),
         )
         self.conv_layer5 = Sequential(
-            torch.nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3),
+            torch.nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3),
             ReLU(),
+            BatchNorm1d(256),
             AdaptiveAvgPool1d(6),
         )
         self.conv_layer6 = Sequential(
-            torch.nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3),
+            torch.nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3),
             ReLU(),
+            BatchNorm1d(512),
             AdaptiveAvgPool1d(1),
-            Dropout(0.00),
+            Dropout(0.05),
         )
-        self.ff_layer1 = Sequential(Linear(128, 128), ReLU())
-        self.ff_layer2 = Sequential(Linear(128, 128), ReLU())
+        self.ff_layer1 = Sequential(Linear(512, 128), BatchNorm1d(128), ReLU())
+        self.ff_layer2 = Sequential(Linear(128, 128), BatchNorm1d(128), ReLU())
 
     def _setup_pre_gnn_layers(self):
         """Sets up pre-GNN dense layers (NOTE: in v0.1 this is always set to 1 layer).
@@ -95,12 +109,39 @@ class CGCNN_CNN(CGCNN):
 
         return pre_lin_list
 
+    def _setup_post_gnn_layers(self):
+        """Sets up post-GNN dense layers (NOTE: in v0.1 there was a minimum of 2 dense layers, and fc_count(now post_fc_count) added to this number. In the current version, the minimum is zero)."""
+        post_lin_list = torch.nn.ModuleList()
+        if self.post_fc_count > 0:
+            for i in range(self.post_fc_count):
+                if i == 0:
+                    # Set2set pooling has doubled dimension
+                    if self.pool_order == "early" and self.pool == "set2set":
+                        lin = Linear(self.post_fc_dim * 2, self.dim2)
+                    else:
+                        lin = Linear(self.post_fc_dim + 128, self.dim2)
+                else:
+                    lin = Linear(self.dim2, self.dim2)
+                post_lin_list.append(lin)
+            lin_out = Linear(self.dim2, self.output_dim)
+            # Set up set2set pooling (if used)
+
+        # else post_fc_count is 0
+        else:
+            if self.pool_order == "early" and self.pool == "set2set":
+                lin_out = Linear(self.post_fc_dim * 2, self.output_dim)
+            else:
+                lin_out = Linear(self.post_fc_dim, self.output_dim)
+
+        return post_lin_list, lin_out
+
     def forward(self, data):
         # get DOS out
-        dos_unscaled = data.scaled * data.scaling_factor.view(-1, 1).expand_as(
+        # dos_unscaled = torch.swapaxes(data.scaled * data.scaling_factor.view(-1, 1, 1).expand_as(data.scaled), 1,2)
+        dos_unscaled = data.scaled * data.scaling_factor.view(-1, 1, 1).expand_as(
             data.scaled
         )
-        dos_out = self.conv_layer1(dos_unscaled.unsqueeze(1))
+        dos_out = self.conv_layer1(dos_unscaled)
         dos_out = self.conv_layer2(dos_out)
         dos_out = self.conv_layer3(dos_out)
         dos_out = self.conv_layer4(dos_out)
@@ -108,6 +149,7 @@ class CGCNN_CNN(CGCNN):
         dos_out = self.conv_layer6(dos_out)
         dos_out = self.ff_layer1(dos_out.squeeze(2))
         dos_out = self.ff_layer2(dos_out)
+        # print(dos_out[0])
         # print(data.scaled.unsqueeze(1).shape, dos_out.shape)
 
         # Pre-GNN dense layers
@@ -120,9 +162,13 @@ class CGCNN_CNN(CGCNN):
             out = self._forward_pre_gnn_layers(
                 torch.cat((data.x.float(), dos_out.squeeze(-1)), 1)
             )
+            # out = self._forward_pre_gnn_layers(data.x.float())
 
         out = self._forward_gnn_layers(data, out)
-        out = self._forward_post_gnn_layers(data, out)
+        # out = self._forward_post_gnn_layers(data, out)
+        out = self._forward_post_gnn_layers(
+            data, torch.cat((out.float(), dos_out.squeeze(-1)), 1)
+        )
 
         out = out.view(-1) if out.shape[1] == 1 else out
         return out
