afe36dd5ade7e07f04a88cd3b9246056ffa3c2df
Rithwik Seth <rithwikseth@gmail.com>
2023-03-31 09:17:34 -0400

    QM9 testing

--- matdeeplearn/preprocessor/helpers.py
+++ matdeeplearn/preprocessor/helpers.py
@@ -476,8 +476,8 @@ def generate_node_features(input_data, n_neighbors, device, use_degree=False):
         # minus 1 as the reps are 0-indexed but atomic number starts from 1
         data.x = node_reps[data.z - 1].view(-1, n_features)
 
-    for i, data in enumerate(input_data):
-        input_data[i] = one_hot_degree(data, n_neighbors)
+    #for i, data in enumerate(input_data):
+        #input_data[i] = one_hot_degree(data, n_neighbors)
 
 
 def generate_edge_features(input_data, edge_steps, r, device):
