0387bfd525933335ec5bea93eee1da5fc89d8845
Sidharth Narayanan Baskaran <sbaskaran31@login-phoenix-slurm-1.pace.gatech.edu>
2023-04-07 18:04:33 -0400

    update config

--- testing/jobs/phoenix.sh
+++ testing/jobs/phoenix.sh
@@ -12,6 +12,6 @@
 cd /storage/home/hcoda1/9/sbaskaran31/p-vfung3-0/MatDeepLearn_dev/scripts
 conda activate matdeeplearn
 
-python main.py --config_path="/nethome/sbaskaran31/projects/Sidharth/MatDeepLearn_dev/configs/examples/cgcnn_vn_hg/config_cgcnn_vn_hg.yml" \
+python main.py --config_path="/storage/home/hcoda1/9/sbaskaran31/p-vfung3-0/MatDeepLearn_dev/configs/examples/cgcnn_vn_hg/config_cgcnn_vn_hg.yml" \
     --run_mode="train" \
     --use_wandb=True
