ddd87fab8696a7c1a4f297badc4a926e2d2614be
Sidharth Narayanan Baskaran <sbaskaran31@login-phoenix-slurm-1.pace.gatech.edu>
2023-04-09 10:53:42 -0400

    update config

--- testing/jobs/phoenix.sh
+++ testing/jobs/phoenix.sh
@@ -3,11 +3,12 @@
 #SBATCH -Agts-vfung3                                # Charge account
 #SBATCH -N1 --gres=gpu:A100:1                       # Number of nodes and GPUs required
 #SBATCH --mem-per-gpu=80G                           # Memory per gpu
-#SBATCH -t12:00:00                                        # Duration of the job (Ex: 15 mins)
+#SBATCH -CA100-80GB                                 # GPU type       
+#SBATCH -t12:00:00                                  # Duration of the job (Ex: 15 mins)
 #SBATCH -qinferno                                   # QOS name
-#SBATCH -ocgcnn_job-%j.out                             # Combined output and error messages file
+#SBATCH -ocgcnn_job-%j.out                          # Combined output and error messages file
 #SBATCH --mail-type=BEGIN,END,FAIL                  # Mail preferences
-#SBATCH --mail-user=sidharth.baskaran@gatech.edu            # e-mail address for notifications
+#SBATCH --mail-user=sidharth.baskaran@gatech.edu    # e-mail address for notifications
 
 cd /storage/home/hcoda1/9/sbaskaran31/p-vfung3-0/MatDeepLearn_dev/scripts
 conda activate matdeeplearn
