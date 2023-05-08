dcee458552f49ed5b126178fc2c8c73339e1c6a5
Rithwik Seth <rithwikseth@gmail.com>
2023-05-08 12:53:08 -0700

    alignnG

--- matdeeplearn/models/alignn_graphite.py
+++ matdeeplearn/models/alignn_graphite.py
@@ -119,12 +119,18 @@ class EGConv(MessagePassing):
 
     def forward(self, x, edge_index, edge_attr):
         i, j = edge_index
-        
+        print(edge_index)
+        print(edge_index.size())  
 
         # Calculate gated edges
         sigma_e = self.sigma(edge_attr)
-        e_sum = scatter(src=sigma_e, index=j, dim=0)
-        e_gated = sigma_e / (e_sum[j] + self.eps)
+        e_sum_i = scatter(src=sigma_e, index=i, dim=0)
+        print(e_sum_i)
+        print(e_sum_i.size())  
+        e_sum_j = scatter(src=sigma_e, index=j, dim=0)
+        print(e_sum_j)
+        print(e_sum_j.size())  
+        e_gated = sigma_e / (e_sum_i[i] + self.eps)
 
         # Update the nodes (this utilizes the gated edges)
         out = self.propagate(edge_index, x=x, e_gated=e_gated)
