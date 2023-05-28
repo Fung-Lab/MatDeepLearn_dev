e82fa44ec71f8736e60443a4bb913d2587798ec4
Rithwik Seth <rithwikseth@gmail.com>
2023-05-28 14:08:39 -0500

    Updating Models to work with new trainers

--- matdeeplearn/models/torchmd_et.py
+++ matdeeplearn/models/torchmd_et.py
@@ -185,6 +185,7 @@ class TorchMD_ET(nn.Module):
         x = self.out_norm(x)
         x = self.pool.pre_reduce(x, vec, data.z, data.pos, data.batch)
         x = self.pool.reduce(x, data.batch)
+        x = x.squeeze()
         if x.shape[1] == 1:
             x = x.view(-1)
 
