00cde4a9b8c6a8c0ec90159a763823433b8182ed
Sarah Eisenach <sarah@eisenach.org>
2023-06-01 11:32:40 -0400

    modifying batch size

--- matdeeplearn/models/cgcnn_resnet_2.py
+++ matdeeplearn/models/cgcnn_resnet_2.py
@@ -48,15 +48,17 @@ class CGCNN_RESNET_2(CGCNN):
 
         # set up CNN
         # self.setup_cnn()
-        self.cnn = ResNet1D(
-            in_channels=3,
-            base_filters=64,
-            kernel_size=3,
-            stride=2,
-            groups=1,
-            n_block=32,
-            n_classes=128,
-        )
+        # self.cnn = ResNet1D(
+        #     in_channels=3,
+        #     base_filters=64,
+        #     kernel_size=3,
+        #     stride=2,
+        #     groups=1,
+        #     n_block=4,
+        #     n_classes=128,
+        # )
+
+        self.cnn = ResNet(BasicBlock, [3, 4, 6, 3])
 
     def setup_cnn(self):
         self.conv_layer1 = Sequential(
@@ -263,53 +265,35 @@ class BasicBlock(nn.Module):
         self,
         in_channels,
         out_channels,
-        kernel_size,
-        stride,
-        groups,
-        downsample,
-        use_bn,
-        use_do,
-        is_first_block=False,
+        stride=1,
+        downsample=None,
     ):
         super(BasicBlock, self).__init__()
 
         self.in_channels = in_channels
-        self.kernel_size = kernel_size
         self.out_channels = out_channels
         self.stride = stride
-        self.groups = groups
         self.downsample = downsample
         if self.downsample:
             self.stride = stride
         else:
             self.stride = 1
-        self.is_first_block = is_first_block
-        self.use_bn = use_bn
-        self.use_do = use_do
 
         # the first conv
-        self.bn1 = nn.BatchNorm1d(in_channels)
-        self.relu1 = nn.ReLU()
-        self.do1 = nn.Dropout(p=0.5)
-        self.conv1 = MyConv1dPadSame(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=self.stride,
-            groups=self.groups,
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(
+                in_channels, out_channels, kernel_size=3, stride=self.stride, padding=1
+            ),
+            nn.BatchNorm1d(out_channels),
+            nn.ReLU(),
         )
 
-        # the second conv
-        self.bn2 = nn.BatchNorm1d(out_channels)
-        self.relu2 = nn.ReLU()
-        self.do2 = nn.Dropout(p=0.5)
-        self.conv2 = MyConv1dPadSame(
-            in_channels=out_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=1,
-            groups=self.groups,
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(out_channels),
         )
+
+        self.relu = nn.ReLU()
 
         self.max_pool = MyMaxPool1dPadSame(kernel_size=self.stride)
 
@@ -317,189 +301,219 @@ class BasicBlock(nn.Module):
 
         identity = x
 
-        # the first conv
-        out = x
-        if not self.is_first_block:
-            if self.use_bn:
-                out = self.bn1(out)
-            out = self.relu1(out)
-            if self.use_do:
-                out = self.do1(out)
-        out = self.conv1(out)
-
-        # the second conv
-        if self.use_bn:
-            out = self.bn2(out)
-        out = self.relu2(out)
-        if self.use_do:
-            out = self.do2(out)
+        out = self.conv1(x)
         out = self.conv2(out)
 
         # if downsample, also downsample identity
         if self.downsample:
-            identity = self.max_pool(identity)
+            identity = self.downsample(identity)
 
-        # if expand channel, also pad zeros to identity
-        if self.out_channels != self.in_channels:
-            identity = identity.transpose(-1, -2)
-            ch1 = (self.out_channels - self.in_channels) // 2
-            ch2 = self.out_channels - self.in_channels - ch1
-            identity = F.pad(identity, (ch1, ch2), "constant", 0)
-            identity = identity.transpose(-1, -2)
+        # # if expand channel, also pad zeros to identity
+        # if self.out_channels != self.in_channels:
+        #     identity = identity.transpose(-1, -2)
+        #     ch1 = (self.out_channels - self.in_channels) // 2
+        #     ch2 = self.out_channels - self.in_channels - ch1
+        #     identity = F.pad(identity, (ch1, ch2), "constant", 0)
+        #     identity = identity.transpose(-1, -2)
 
         # shortcut
         out += identity
+        out = self.relu(out)
 
         return out
 
 
-class ResNet1D(nn.Module):
-    """
-
-    Input:
-        X: (n_samples, n_channel, n_length)
-        Y: (n_samples)
-
-    Output:
-        out: (n_samples)
-
-    Pararmetes:
-        in_channels: dim of input, the same as n_channel
-        base_filters: number of filters in the first several Conv layer, it will double at every 4 layers
-        kernel_size: width of kernel
-        stride: stride of kernel moving
-        groups: set larget to 1 as ResNeXt
-        n_block: number of blocks
-        n_classes: number of classes
-
-    """
-
-    def __init__(
-        self,
-        in_channels,
-        base_filters,
-        kernel_size,
-        stride,
-        groups,
-        n_block,
-        n_classes,
-        downsample_gap=2,
-        increasefilter_gap=4,
-        use_bn=True,
-        use_do=True,
-        verbose=False,
-    ):
-        super(ResNet1D, self).__init__()
-
-        self.verbose = verbose
-        self.n_block = n_block
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.groups = groups
-        self.use_bn = use_bn
-        self.use_do = use_do
-
-        self.downsample_gap = downsample_gap  # 2 for base model
-        self.increasefilter_gap = increasefilter_gap  # 4 for base model
-
-        # first block
-        self.first_block_conv = MyConv1dPadSame(
-            in_channels=in_channels,
-            out_channels=base_filters,
-            kernel_size=self.kernel_size,
-            stride=1,
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=128):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
         )
-        self.first_block_bn = nn.BatchNorm1d(base_filters)
-        self.first_block_relu = nn.ReLU()
-        out_channels = base_filters
+        self.maxpool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
+        # self.maxpool = MyMaxPool1dPadSame(kernel_size=self.stride)
+        self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(512, num_classes)
 
-        # residual blocks
-        self.basicblock_list = nn.ModuleList()
-        for i_block in range(self.n_block):
-            # is_first_block
-            if i_block == 0:
-                is_first_block = True
-            else:
-                is_first_block = False
-            # downsample at every self.downsample_gap blocks
-            if i_block % self.downsample_gap == 1:
-                downsample = True
-            else:
-                downsample = False
-            # in_channels and out_channels
-            if is_first_block:
-                in_channels = base_filters
-                out_channels = in_channels
-            else:
-                # increase filters at every self.increasefilter_gap blocks
-                in_channels = int(
-                    base_filters * 2 ** ((i_block - 1) // self.increasefilter_gap)
-                )
-                if (i_block % self.increasefilter_gap == 0) and (i_block != 0):
-                    out_channels = in_channels * 2
-                else:
-                    out_channels = in_channels
-
-            tmp_block = BasicBlock(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                kernel_size=self.kernel_size,
-                stride=self.stride,
-                groups=self.groups,
-                downsample=downsample,
-                use_bn=self.use_bn,
-                use_do=self.use_do,
-                is_first_block=is_first_block,
+    def _make_layer(self, block, channels, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != channels:
+            downsample = nn.Sequential(
+                nn.Conv1d(self.in_channels, channels, kernel_size=1, stride=stride),
+                nn.BatchNorm1d(channels),
             )
-            self.basicblock_list.append(tmp_block)
+        layers = []
+        layers.append(block(self.in_channels, channels, stride, downsample))
+        self.in_channels = channels
+        for i in range(1, blocks):
+            layers.append(block(self.in_channels, channels))
 
-        # final prediction
-        self.final_bn = nn.BatchNorm1d(out_channels)
-        self.final_relu = nn.ReLU(inplace=True)
-        # self.do = nn.Dropout(p=0.5)
-        self.dense = nn.Linear(out_channels, n_classes)
-        # self.softmax = nn.Softmax(dim=1)
+        return nn.Sequential(*layers)
 
     def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
 
-        out = x
+        x = self.avgpool(x)
+        # x = x.view(x.size(0), -1)
+        x = self.fc(x.squeeze(2))
 
-        # first conv
-        if self.verbose:
-            print("input shape", out.shape)
-        out = self.first_block_conv(out)
-        if self.verbose:
-            print("after first conv", out.shape)
-        if self.use_bn:
-            out = self.first_block_bn(out)
-        out = self.first_block_relu(out)
+        return x
 
-        # residual blocks, every block has two conv
-        for i_block in range(self.n_block):
-            net = self.basicblock_list[i_block]
-            if self.verbose:
-                print(
-                    "i_block: {0}, in_channels: {1}, out_channels: {2}, downsample: {3}".format(
-                        i_block, net.in_channels, net.out_channels, net.downsample
-                    )
-                )
-            out = net(out)
-            if self.verbose:
-                print(out.shape)
 
-        # final prediction
-        if self.use_bn:
-            out = self.final_bn(out)
-        out = self.final_relu(out)
-        out = out.mean(-1)
-        if self.verbose:
-            print("final pooling", out.shape)
-        # out = self.do(out)
-        out = self.dense(out)
-        if self.verbose:
-            print("dense", out.shape)
-        # out = self.softmax(out)
-        if self.verbose:
-            print("softmax", out.shape)
-
-        return out
+# class ResNet1D(nn.Module):
+#     """
+#
+#     Input:
+#         X: (n_samples, n_channel, n_length)
+#         Y: (n_samples)
+#
+#     Output:
+#         out: (n_samples)
+#
+#     Pararmetes:
+#         in_channels: dim of input, the same as n_channel
+#         base_filters: number of filters in the first several Conv layer, it will double at every 4 layers
+#         kernel_size: width of kernel
+#         stride: stride of kernel moving
+#         groups: set larget to 1 as ResNeXt
+#         n_block: number of blocks
+#         n_classes: number of classes
+#
+#     """
+#
+#     def __init__(
+#         self,
+#         in_channels,
+#         base_filters,
+#         kernel_size,
+#         stride,
+#         groups,
+#         n_block,
+#         n_classes,
+#         downsample_gap=2,
+#         increasefilter_gap=4,
+#         use_bn=True,
+#         use_do=True,
+#         verbose=False,
+#     ):
+#         super(ResNet1D, self).__init__()
+#
+#         self.verbose = verbose
+#         self.n_block = n_block
+#         self.kernel_size = kernel_size
+#         self.stride = stride
+#         self.groups = groups
+#         self.use_bn = use_bn
+#         self.use_do = use_do
+#
+#         self.downsample_gap = downsample_gap  # 2 for base model
+#         self.increasefilter_gap = increasefilter_gap  # 4 for base model
+#
+#         # first block
+#         self.first_block = nn.Sequential(
+#             MyConv1dPadSame(
+#                 in_channels=in_channels,
+#                 out_channels=base_filters,
+#                 kernel_size=self.kernel_size,
+#                 stride=1,
+#             ),
+#             nn.BatchNorm1d(base_filters),
+#             nn.ReLU(),
+#         )
+#
+#         out_channels = base_filters
+#
+#         # residual blocks
+#         self.basicblock_list = nn.ModuleList()
+#         for i_block in range(self.n_block):
+#             # is_first_block
+#             if i_block == 0:
+#                 is_first_block = True
+#             else:
+#                 is_first_block = False
+#             # downsample at every self.downsample_gap blocks
+#             if i_block % self.downsample_gap == 1:
+#                 downsample = True
+#             else:
+#                 downsample = False
+#             # in_channels and out_channels
+#             if is_first_block:
+#                 in_channels = base_filters
+#                 out_channels = in_channels
+#             else:
+#                 # increase filters at every self.increasefilter_gap blocks
+#                 in_channels = int(
+#                     base_filters * 2 ** ((i_block - 1) // self.increasefilter_gap)
+#                 )
+#                 if (i_block % self.increasefilter_gap == 0) and (i_block != 0):
+#                     out_channels = in_channels * 2
+#                 else:
+#                     out_channels = in_channels
+#
+#             tmp_block = BasicBlock(
+#                 in_channels=in_channels,
+#                 out_channels=out_channels,
+#                 kernel_size=self.kernel_size,
+#                 stride=self.stride,
+#                 groups=self.groups,
+#                 downsample=downsample,
+#                 use_bn=self.use_bn,
+#                 use_do=self.use_do,
+#                 is_first_block=is_first_block,
+#             )
+#             self.basicblock_list.append(tmp_block)
+#
+#         # final prediction
+#         self.final_bn = nn.BatchNorm1d(out_channels)
+#         self.final_relu = nn.ReLU(inplace=True)
+#         # self.do = nn.Dropout(p=0.5)
+#         self.dense = nn.Linear(out_channels, n_classes)
+#         # self.softmax = nn.Softmax(dim=1)
+#
+#     def forward(self, x):
+#
+#         out = x
+#
+#         # first conv
+#         out = self.first_block(out)
+#
+#         # residual blocks, every block has two conv
+#         for i_block in range(self.n_block):
+#             net = self.basicblock_list[i_block]
+#             if self.verbose:
+#                 print(
+#                     "i_block: {0}, in_channels: {1}, out_channels: {2}, downsample: {3}".format(
+#                         i_block, net.in_channels, net.out_channels, net.downsample
+#                     )
+#                 )
+#             out = net(out)
+#             if self.verbose:
+#                 print(out.shape)
+#
+#         # final prediction
+#         if self.use_bn:
+#             out = self.final_bn(out)
+#         out = self.final_relu(out)
+#         out = out.mean(-1)
+#         if self.verbose:
+#             print("final pooling", out.shape)
+#         # out = self.do(out)
+#         out = self.dense(out)
+#         if self.verbose:
+#             print("dense", out.shape)
+#         # out = self.softmax(out)
+#         if self.verbose:
+#             print("softmax", out.shape)
+#
+#         return out
