927dcbf8ba9b5ebd2071753ad00988fb535fb0db
Fan Shu <fanshu0102@gmail.com>
2023-06-02 16:39:08 -0400

    created finetune model following CT paper

--- matdeeplearn/models/cgcnn_ct_finetune.py
+++ matdeeplearn/models/cgcnn_ct_finetune.py
@@ -60,10 +60,10 @@ class CGCNNCTPretrain(BaseModel):
             self.gc_dim, self.post_fc_dim = dim1, dim1
 
         # Determine output dimension length
-        if data[0][0][self.target_attr].ndim == 0:
+        if data[0][self.target_attr].ndim == 0:
             self.output_dim = 1
         else:
-            self.output_dim = len(data[0][0][self.target_attr])
+            self.output_dim = len(data[0][self.target_attr])
 
         # setup layers
         self.pre_lin_list = self._setup_pre_gnn_layers()
