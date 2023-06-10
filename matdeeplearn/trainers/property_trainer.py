084d8b2bea12b590aa0ffae3b4b3e0191e4d4ff0
Rithwik Seth <rithwikseth@gmail.com>
2023-06-10 13:47:41 -0500

    Updating models

--- matdeeplearn/trainers/property_trainer.py
+++ matdeeplearn/trainers/property_trainer.py
@@ -215,6 +215,7 @@ class PropertyTrainer(BaseTrainer):
         for i in range(0, len(loader_iter)):
             batch = next(loader_iter).to(self.rank)      
             out = self._forward(batch.to(self.rank))
+            out["output"] = out["output"].squeeze()
             batch_p = out["output"].data.cpu().numpy()
             batch_ids = batch.structure_id 
             
