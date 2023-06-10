a5871fb901f109429e3fe56f8965dad96ea03162
Rithwik Seth <rithwikseth@gmail.com>
2023-06-10 15:08:26 -0500

    Updating models

--- matdeeplearn/trainers/property_trainer.py
+++ matdeeplearn/trainers/property_trainer.py
@@ -324,7 +324,6 @@ class PropertyTrainer(BaseTrainer):
         return output
 
     def _compute_loss(self, out, batch_data):
-        print(out["output"].size())
         loss = self.loss_fn(out, batch_data)
         return loss
 
