2a221723438b019b0c0f99458ac3b1c311d65485
Sarah Eisenach <sarah@eisenach.org>
2023-06-27 14:32:19 -0400

    fixing spd dim

--- matdeeplearn/preprocessor/transforms.py
+++ matdeeplearn/preprocessor/transforms.py
@@ -41,7 +41,8 @@ class GetY(object):
                 data.y = data.y.unsqueeze(0)
             # print("2", data.y.shape)
             # data.y = data.y[0][self.index]
-        data.y = data.scaled
+        data.y = data.scaled.reshape(data.scaled.shape[0], 1200)
+
         data.x = data.x.float()
         return data
 
