84fb9756362110a3caf46224b25f7d2f1ef6e952
Fan Shu <fanshu0102@gmail.com>
2023-07-13 01:00:20 -0400

    GeoSSL finetune

---
+++ scripts/ft/geossl/run_scratch.py
@@ -0,0 +1,36 @@
+import time
+import subprocess
+import os
+
+# 运行次数
+num_runs = 5
+
+if __name__ == '__main__':
+
+    # 循环执行
+    for i in range(num_runs):
+
+        # 执行命令行命令
+        command = "python scripts/main.py --run_mode=train --config_path=configs/ct_ft/geossl/config_JDFT.yml"
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/ct_ft/geossl/config_Dielectric.yml"
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/ct_ft/geossl/config_Perovskites.yml"
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/ct_ft/geossl/config_GVRH.yml"
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/ct_ft/geossl/config_KVRH.yml"
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/ct_ft/geossl/config_Phonons.yml"
+        subprocess.run(command, shell=True)
+
+
+
+        # 记录本次运行的开始时间
+        # current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        # print(f"Run {i + 1} completed at {current_time}")
