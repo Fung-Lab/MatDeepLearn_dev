0de05a74bf16236051d66739f4421763f0a9af33
Fan Shu <fanshu0102@gmail.com>
2023-08-21 00:08:14 -0400

    finetune for anneal 5 0.05 noise level 50

---
+++ scripts/ft/geossl/run_level50.py
@@ -0,0 +1,36 @@
+import time
+import subprocess
+import os
+import random
+
+# 运行次数
+num_runs = 5
+seeds = [123, 456, 789, 987, 543, 321, 876, 654, 234, 345]
+
+if __name__ == '__main__':
+
+    # 循环执行
+    for i in range(num_runs):
+        seed = str(seeds[i])
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/geossl/GeoSSL_noise50/config_JDFT_ct_ft.yml --seed=" + seed
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/geossl/GeoSSL_noise50/config_Dielectric_ct_ft.yml --seed=" + seed
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/geossl/GeoSSL_noise50/config_Perovskites_ct_ft.yml --seed=" + seed
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/geossl/GeoSSL_noise50/config_Phonons_ct_ft.yml --seed=" + seed
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/geossl/GeoSSL_noise50/config_GVRH_ct_ft.yml --seed=" + seed
+        subprocess.run(command, shell=True)
+
+        command = "python scripts/main.py --run_mode=train --config_path=configs/geossl/GeoSSL_noise50/config_KVRH_ct_ft.yml --seed=" + seed
+        subprocess.run(command, shell=True)
+
+        # 记录本次运行的开始时间
+        # current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        # print(f"Run {i + 1} completed at {current_time}")
