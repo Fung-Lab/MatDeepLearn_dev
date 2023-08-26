2e7c849523cafd9437fd4e39a186d9e892569a03
Rithwik Seth <rithwikseth@gmail.com>
2023-08-26 17:57:08 -0400

    Adding crystal graph

--- matdeeplearn/preprocessor/transforms.py
+++ matdeeplearn/preprocessor/transforms.py
@@ -102,15 +102,10 @@ class CrystalGraphMod(object):
             data.nbr_fea = torch.empty(0, self.neighbors, 50)
             data.nbr_fea = torch.cat((data.nbr_fea, a), 0)
         try:
-            data.crystal_atom_idx.append(torch.arange(data.crystal_atom_idx[-1][-1]+1, data.crystal_atom_idx[-1][-1]+1+data.n_atoms))
-            print(data.crystal_atom_idx[-1])
-            print(torch.arange(data.crystal_atom_idx[-1][-1]+1, data.crystal_atom_idx[-1][-1]+1+data.n_atoms))
+            data.crystal_atom_idx.append(torch.arange(data.crystal_atom_idx[-1][-1].item()+1, data.crystal_atom_idx[-1][-1].item()+1+data.n_atoms))
         except:
             data.crystal_atom_idx = []
             data.crystal_atom_idx.append(torch.arange(0, data.n_atoms))
-            print(data.crystal_atom_idx)
-            print(torch.arange(0, data.n_atoms))
-            print(data.crystal_atom_idx[-1][-1])
     
         return data
 
