8943a6447f6674d0d5ba6d4bd08c167a9df47599
Fan Shu <fanshu0102@gmail.com>
2023-09-01 09:19:53 -0400

    output sampled vn_labels and original vn_labels

--- matdeeplearn/preprocessor/processor.py
+++ matdeeplearn/preprocessor/processor.py
@@ -82,8 +82,10 @@ def process_data(dataset_config):
 
 
 def get_sampling_indices(vn_labels, num_samples):
+    print(vn_labels)
     prob_distribution = vn_labels / np.sum(vn_labels)
     sampled_indices = np.random.choice(len(vn_labels), size=num_samples, p=prob_distribution.squeeze())
+    print(vn_labels[sampled_indices, :])
 
     return sampled_indices
 
