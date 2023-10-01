2062c6defda32232115c1cd4c33a7a4bc35533b6
rithwiks <rseth34@gatech.edu>
2023-10-01 17:58:58 -0400

    Geomtric implementation of message passing for crystal graph

---
+++ matdeeplearn/models/model_myGeo.py
@@ -0,0 +1,244 @@
+from __future__ import print_function, division
+
+import torch
+import numpy as np
+import torch.nn as nn
+from matdeeplearn.models.base_model import BaseModel, conditional_grad
+from matdeeplearn.common.registry import registry
+from torch_geometric.nn import (
+    global_mean_pool,
+    MessagePassing,
+)
+import warnings
+
+warnings.filterwarnings("ignore")
+
+#in comparison to my2, here we use softmax rather than sigmoid
+#we also have the choice to use max pool rather than mean pool
+class ConvLayer(MessagePassing):
+    """
+    Convolutional operation on graphs
+    """
+    def __init__(self, atom_fea_len, nbr_fea_len, k=3):
+        """
+        Initialize ConvLayer.
+
+        Parameters
+        ----------
+
+        atom_fea_len: int
+          Number of atom hidden features.
+        nbr_fea_len: int
+          Number of bond features.
+        """
+        super(ConvLayer, self).__init__()
+        self.atom_fea_len = atom_fea_len
+        self.nbr_fea_len = nbr_fea_len
+        self.k = k
+        self.fc_f=nn.ModuleList([nn.Linear(2*self.atom_fea_len+self.nbr_fea_len,
+                                 2*self.atom_fea_len+self.nbr_fea_len) for i in range(k)])
+        self.fc_s=nn.ModuleList([nn.Linear(2*self.atom_fea_len+self.nbr_fea_len,
+                                 2*self.atom_fea_len+self.nbr_fea_len) for i in range(k)])
+        self.softmax= nn.Softmax(dim=1)
+        self.softmax2= nn.Softmax(dim=2)
+        self.softmax3= nn.Softmax(dim=3)
+        self.softplus1 = nn.ReLU()
+        self.softplus2 = nn.ReLU()
+        self.softplus3 = nn.ReLU()
+        self.bn1s = nn.ModuleList([nn.BatchNorm1d(2*self.atom_fea_len+self.nbr_fea_len) for i in range(k)])
+        self.bn2s =  nn.ModuleList([nn.BatchNorm1d(self.atom_fea_len) for i in range(k)])
+        self.softplus2s = nn.ModuleList([nn.ReLU() for i in range(k)])
+        self.softplus3s = nn.ModuleList([nn.ReLU() for i in range(k)])
+        self.atom_fc = nn.Linear(self.k , 2*self.k) 
+        self.nbr_fc = nn.Linear(self.k , 2*self.k) 
+        self.dropout = nn.Dropout()
+
+    def forward(self, x, edge_index, distances):
+        return self.propagate(edge_index, x=x, distances=distances)
+
+        
+    def message(self, x_i, x_j, distances):
+        z = torch.cat([x_i, x_j, distances], dim=-1)
+        out = torch.zeros_like(z)
+        for lin_f, lin_s, bn in zip(self.fc_f, self.fc_s):
+            out += self.softmax(lin_f(z)) * self.softplus1(lin_s(z))
+        out /= self.k
+        node_msgs = z[:, :2 * self.atom_fea_len]
+        edge_msgs = z[:, 2 * self.atom_fea_len:]
+        return node_msgs, edge_msgs
+    
+    def update(self, aggr_out):
+        node_msgs, edge_msgs = aggr_out
+        new_node_features = node_msgs.sum(dim=1)
+        new_edge_features = edge_msgs.sum(dim=1)
+
+        return new_node_features, new_edge_features
+
+            
+
+
+@registry.register_model("CrystalGraphGeo")
+class CrystalGraphConvNet(BaseModel):
+    """
+    Create a crystal graph convolutional neural network for predicting total
+    material properties.
+    """
+    def __init__(self, node_dim, edge_dim, output_dim,
+                 dim1=128, n_conv=9, dim2=128, n_h=1,k=3,
+                 classification=False, **kwargs):
+        """
+        Initialize CrystalGraphConvNet.
+
+        Parameters
+        ----------
+
+        node_dim: int
+          Number of atom features in the input.
+        edge_dim: int
+          Number of bond features.
+        dim1: int
+          Number of hidden atom features in the convolutional layers
+        n_conv: int
+          Number of convolutional layers
+        dim2: int
+          Number of hidden features after pooling
+        n_h: int
+          Number of hidden layers after pooling
+        """
+        super(CrystalGraphConvNet, self).__init__()
+        self.classification = classification
+        self.embedding = nn.Linear(node_dim, dim1)
+        self.convs = nn.ModuleList([ConvLayer(atom_fea_len=dim1,
+                                    nbr_fea_len=edge_dim,k=k)
+                                    for _ in range(n_conv)])
+        self.conv_to_fc = nn.Linear(dim1, dim2)
+        self.conv_to_fc_softplus = nn.ReLU()
+        self.output_softplus= nn.ReLU()
+        if n_h > 1:
+            self.fcs = nn.ModuleList([nn.Linear(dim2, dim2)
+                                      for _ in range(n_h-1)])
+            self.softpluses = nn.ModuleList([nn.ReLU()
+                                             for _ in range(n_h-1)])
+        if self.classification:
+            self.fc_out = nn.Linear(dim2, 2)
+        else:
+            self.fc_out = nn.Linear(dim2, output_dim)
+        if self.classification:
+            self.logsoftmax = nn.LogSoftmax(dim=1)
+            self.dropout = nn.Dropout()
+    def forward(self, data):
+    
+        output = {}
+        out = self._forward(data)
+        output["output"] =  out
+
+        if self.gradient == True and out.requires_grad == True:         
+            if self.gradient_method == "conventional":
+                volume = torch.einsum("zi,zi->z", data.cell[:, 0, :], torch.cross(data.cell[:, 1, :], data.cell[:, 2, :], dim=1)).unsqueeze(-1)                        
+                grad = torch.autograd.grad(
+                        out,
+                        [data.pos, data.cell],
+                        grad_outputs=torch.ones_like(out),
+                        create_graph=self.training) 
+                forces = -1 * grad[0]
+                stress = grad[1] 
+                stress = stress / volume.view(-1, 1, 1)
+            #For calculation of stress, see https://github.com/mir-group/nequip/blob/main/nequip/nn/_grad_output.py
+            #Originally from: https://github.com/atomistic-machine-learning/schnetpack/issues/165                              
+            elif self.gradient_method == "nequip":
+                volume = torch.einsum("zi,zi->z", data.cell[:, 0, :], torch.cross(data.cell[:, 1, :], data.cell[:, 2, :], dim=1)).unsqueeze(-1)                        
+                grad = torch.autograd.grad(
+                        out,
+                        [data.pos, data.displacement],
+                        grad_outputs=torch.ones_like(out),
+                        create_graph=self.training) 
+                forces = -1 * grad[0]
+                stress = grad[1]
+                stress = stress / volume.view(-1, 1, 1)         
+
+            output["pos_grad"] =  forces
+            output["cell_grad"] =  stress
+        else:
+            output["pos_grad"] =  None
+            output["cell_grad"] =  None  
+                  
+        return output
+    @conditional_grad(torch.enable_grad())
+    def _forward(self, data):#atom_fea, nbr_fea, nbr_fea_idx, crystal_atom_idx):
+        """
+        Forward pass
+
+        N: Total number of atoms in the batch
+        M: Max number of neighbors
+        N0: Total number of crystals in the batch
+
+        Parameters
+        ----------
+
+        atom_fea: Variable(torch.Tensor) shape (N, orig_atom_fea_len)
+          Atom features from atom type
+        
+        nbr_fea: Variable(torch.Tensor) shape (N, M, nbr_fea_len)
+          Bond features of each atom's M neighbors
+        nbr_fea_idx: torch.LongTensor shape (N, M)
+          Indices of M neighbors of each atom
+        crystal_atom_idx: list of torch.LongTensor of length N0
+          Mapping from the crystal idx to atom idx
+
+        Returns
+        -------
+
+        prediction: nn.Variable shape (N, )
+          Atom hidden features after convolution
+
+        """
+        atom_fea = data.x
+        edge_index = data.edge_index
+        distances = data.distances
+        atom_fea = self.embedding(atom_fea)
+        for conv_func in self.convs:
+            atom_fea, distances = conv_func(atom_fea, data.edge_index, distances)
+            #print('IN FORWARD, atom_fea size', atom_fea.size())
+        #crys_fea = self.pooling(atom_fea, crystal_atom_idx)
+        crys_fea = global_mean_pool(atom_fea, data.batch)
+        crys_fea = self.conv_to_fc(self.conv_to_fc_softplus(crys_fea))
+        crys_fea = self.conv_to_fc_softplus(crys_fea)
+        if self.classification:
+            crys_fea = self.dropout(crys_fea)
+        if hasattr(self, 'fcs') and hasattr(self, 'softpluses'):
+            for fc, softplus in zip(self.fcs, self.softpluses):
+                crys_fea = softplus(fc(crys_fea))
+        out = self.fc_out(crys_fea)
+        if self.classification:
+            out = self.logsoftmax(out)
+        return out
+    @property
+    def target_attr(self):
+        return "y"
+
+    def pooling(self, atom_fea, crystal_atom_idx):
+        """
+        Pooling the atom features to crystal features
+
+        N: Total number of atoms in the batch
+        N0: Total number of crystals in the batch
+
+        Parameters
+        ----------
+
+        atom_fea: Variable(torch.Tensor) shape (N, atom_fea_len)
+          Atom feature vectors of the batch
+        crystal_atom_idx: list of torch.LongTensor of length N0
+          Mapping from the crystal idx to atom idx
+        """
+        assert sum([len(idx_map) for idx_map in crystal_atom_idx]) ==\
+            atom_fea.data.shape[0]
+        #print('In POOLING', atom_fea.data.shape[0])
+        summed_fea = [torch.mean(atom_fea[idx_map], dim=0, keepdim=True) for idx_map in crystal_atom_idx]
+        #summed_fea = [torch.max(atom_fea[idx_map],dim=0, keepdim=True)[0] for idx_map in crystal_atom_idx]
+        #print('In POOLING, summed_fea ', len(summed_fea))
+        #print('In POOLING, crystal_atom_idx', len(crystal_atom_idx))
+        #a = torch.Tensor(summed_fea)
+        a = torch.cat(summed_fea, dim=0)
+        #print('return tensor size', a.size())
+        return a
