0b22c20799b29782357dab960cf6e6cf4af7fc9b
rithwiks <rseth34@gatech.edu>
2023-10-11 20:40:30 -0400

    Modifing crystalgraph to match previous implementation

--- matdeeplearn/models/crystalgraph.py
+++ matdeeplearn/models/crystalgraph.py
@@ -54,25 +54,44 @@ class ConvLayer(MessagePassing):
 
     def forward(self, x, edge_index, distances):
         self.new_nbrs = []
+        self.outs = []
+        for i in range(self.k):
+            self.outs.append(torch.zeros(x.size()).to(x.get_device()))
+            self.new_nbrs.append(distances)
         self.nbr_fea = distances
-        return self.propagate(edge_index, x=x, distances=distances), self.nbr_fea
+        self.propagate(edge_index, x=x, distances=distances)
+        out=torch.stack(self.outs, dim=2)
+        new_nbr=torch.stack(self.new_nbrs, dim=3)
+        
+        out_gated=self.atom_fc(out)
+        new_nbr_gated=self.nbr_fc(new_nbr)
+       
+        out_core, out_filter = out_gated.split([self.k, self.k], dim=2) 
+        new_nbr_core, new_nbr_filter = new_nbr_gated.split([self.k, self.k], dim=2)
+        out_filter=self.softmax2(out_filter)
+        new_nbr_filter=self.softmax3(new_nbr_filter)
+        out = torch.sum(out_core * out_filter, dim=2)
+        new_nbr = torch.sum(new_nbr_core* new_nbr_filter, dim=3)
+        return out, new_nbr
 
 
         
     def message(self, x_i, x_j, distances):
         z = torch.cat([x_i, x_j, distances], dim=-1)
-        temp = torch.zeros(distances.size()[0], self.atom_fea_len).to(x_i.get_device())
-        for fc, bn1, bn2, softplus2, softplus3 in zip(self.fc_fulls, self.bn1s, self.bn2s, self.softplus2s, self.softplus3s) :
+        #temp = torch.zeros(distances.size()[0], self.atom_fea_len).to(x_i.get_device())
+        for fc, bn1, bn2, softplus2, softplus3, i in zip(self.fc_fulls, self.bn1s, self.bn2s, self.softplus2s, self.softplus3s, self.k) :
             total_gated_fea = fc(z)
             total_gated_fea = bn1(total_gated_fea)#.view(-1, self.atom_fea_len*2+self.nbr_fea_len)).view(self.N, self.M, self.atom_fea_len*2+self.nbr_fea_len)
             nbr_filter, nbr_core, new_nbr = total_gated_fea.split([self.atom_fea_len,self.atom_fea_len,self.nbr_fea_len], dim=1)
             nbr_filter = self.softmax(nbr_filter)
             nbr_core = self.softplus1(nbr_core) 
             #nbr_sumed = torch.sum(nbr_filter * nbr_core, dim=1)
-            nbr_sumed = bn2(nbr_filter * nbr_core)
-            temp = temp + nbr_sumed
-            self.nbr_fea += new_nbr
-        return temp
+            nbr_sumed = nbr_filter * nbr_core
+            #temp = temp + nbr_sumed
+            self.outs[i] += nbr_sumed
+            self.new_nbrs[i] += new_nbr
+            #self.nbr_fea += new_nbr
+        return x_i
     
 
             
