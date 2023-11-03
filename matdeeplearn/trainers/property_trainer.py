c72d71060c07d00fcba294fc680c2b721b65127e
Fan Shu <fanshu0102@gmail.com>
2023-11-03 01:50:54 -0400

    modify processor to do whole pred

--- matdeeplearn/trainers/property_trainer.py
+++ matdeeplearn/trainers/property_trainer.py
@@ -110,7 +110,8 @@ class PropertyTrainer(BaseTrainer):
                     out = self._forward(batch)
                     loss = self._compute_loss(out, batch)
                     # print(i, torch.cuda.memory_allocated() / (1024 * 1024), torch.cuda.memory_cached() / (1024 * 1024))
-                grad_norm = self._backward(loss)
+                grad_norm = self._backward(loss +
+                                           0.1 * torch.nn.functional.l1_loss(torch.sum(out["output"]), torch.sum(batch.y)))
                 pbar.set_description("Batch Loss {:.4f}, grad norm {:.4f}".format(loss.item(), grad_norm.item()))
                 # Compute metrics
                 # TODO: revert _metrics to be empty per batch, so metrics are logged per batch, not per epoch
