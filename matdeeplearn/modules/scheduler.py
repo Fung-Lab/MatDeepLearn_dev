3210c354ab8b6d20fe1f1d7c287702783e69e742
Fan Shu <fanshu0102@gmail.com>
2024-02-24 01:31:40 -0500

    equiformerv2 -- lr 0.0004

--- matdeeplearn/modules/scheduler.py
+++ matdeeplearn/modules/scheduler.py
@@ -1,4 +1,26 @@
+import math
 import torch
+
+
+class CosineLRLambda:
+    def __init__(self, scheduler_params) -> None:
+        self.warmup_epochs = scheduler_params["warmup_epochs"]
+        self.lr_warmup_factor = scheduler_params["warmup_factor"]
+        self.max_epochs = scheduler_params["epochs"]
+        self.lr_min_factor = scheduler_params["lr_min_factor"]
+
+    def __call__(self, current_step: int):
+        # `warmup_epochs` is already multiplied with the num of iterations
+        if current_step <= self.warmup_epochs:
+            alpha = current_step / float(self.warmup_epochs)
+            return self.lr_warmup_factor * (1.0 - alpha) + alpha
+        else:
+            if current_step >= self.max_epochs:
+                return self.lr_min_factor
+            lr_scale = self.lr_min_factor + 0.5 * (1 - self.lr_min_factor) * (
+                    1 + math.cos(math.pi * (current_step / self.max_epochs))
+            )
+            return lr_scale
 
 
 class LRScheduler:
@@ -7,6 +29,18 @@ class LRScheduler:
     def __init__(self, optimizer, scheduler_type, model_parameters):
         self.optimizer = optimizer
         self.scheduler_type = scheduler_type
+
+        if self.scheduler_type == "LambdaLR":
+            scheduler_lambda_fn = None
+            self.lambda_type = model_parameters["lambda_type"]
+
+            if self.lambda_type == "cosine":
+                scheduler_lambda_fn = CosineLRLambda(model_parameters)
+            # elif self.lambda_type == "multistep":
+            #     scheduler_lambda_fn = MultistepLRLambda(self.scheduler_params)
+            else:
+                raise ValueError
+            model_parameters["lr_lambda"] = scheduler_lambda_fn
 
         self.scheduler = getattr(torch.optim.lr_scheduler, self.scheduler_type)(
             optimizer, **model_parameters
