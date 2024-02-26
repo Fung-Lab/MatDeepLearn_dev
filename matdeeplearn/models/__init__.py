2bf8810a4f6621288b612c4dc8c3a94dc7c0afce
Victor Fung <victorfung88@hotmail.com>
2024-02-25 21:24:12 -0500

    Update __init__.py

--- matdeeplearn/models/__init__.py
+++ matdeeplearn/models/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ["BaseModel", "CGCNN", "MPNN", "SchNet", "TorchMD_ET"]
+__all__ = ["BaseModel", "CGCNN", "MPNN", "SchNet", "TorchMD_ET", "TorchMD_ET_early"]
 
 from .base_model import BaseModel
 from .cgcnn import CGCNN
 from .mpnn import MPNN
 from .schnet import SchNet
-from .torchmd_etEarly import TorchMD_ET
+from .torchmd_et import TorchMD_ET
+from .torchmd_etEarly import TorchMD_ET_Early
