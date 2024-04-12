d020561a01223842dcdbbe5ec0e4fce897ec5089
iamaray <arya.nahri@gmail.com>
2024-04-11 20:11:56 -0400

    stuff

--- graph_structure_learning/graph_structure_learner.py
+++ graph_structure_learning/graph_structure_learner.py
@@ -2,9 +2,13 @@
     Performs graph structure learning on the MSG as outlined in the
     paper.
 """
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class GraphStructureLearner:
+class GraphStructureLearner(nn.Module):
     """
         Implements GSL.
     """
