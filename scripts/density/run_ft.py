4741c238ec6cdbb4259cd3065fd9e3cde12e4862
Fan Shu <fanshu0102@gmail.com>
2024-04-15 17:58:03 -0400

    equiformer finetune -- modify finetune script

--- scripts/density/run_ft.py
+++ scripts/density/run_ft.py
@@ -11,20 +11,21 @@ if __name__ == '__main__':
     for i in range(num_runs):
         seed = str(seeds[i])
 
-        command = "python scripts/main.py --config_path=configs/finetune/config_JDFT.yml --seed=" + seed
-        subprocess.run(command, shell=True)
+        # command = "python scripts/main.py --config_path=configs/finetune/config_JDFT.yml --seed=" + seed
+        # subprocess.run(command, shell=True)
 
         command = "python scripts/main.py --config_path=configs/finetune/config_Dielectric.yml --seed=" + seed
         subprocess.run(command, shell=True)
 
-        command = "python scripts/main.py --config_path=configs/finetune/config_Perovskites.yml --seed=" + seed
-        subprocess.run(command, shell=True)
+        # command = "python scripts/main.py --config_path=configs/finetune/config_Perovskites.yml --seed=" + seed
+        # subprocess.run(command, shell=True)
+        #
+        # command = "python scripts/main.py --config_path=configs/finetune/config_Phonons.yml --seed=" + seed
+        # subprocess.run(command, shell=True)
 
-        command = "python scripts/main.py --config_path=configs/finetune/config_Phonons.yml --seed=" + seed
-        subprocess.run(command, shell=True)
-
-        command = "python scripts/main.py --config_path=configs/finetune/config_GVRH.yml --seed=" + seed
-        subprocess.run(command, shell=True)
+        if i == 0:
+            command = "python scripts/main.py --config_path=configs/finetune/config_GVRH.yml --seed=" + seed
+            subprocess.run(command, shell=True)
 
         command = "python scripts/main.py --config_path=configs/finetune/config_KVRH.yml --seed=" + seed
         subprocess.run(command, shell=True)
