140f9251c7740edf1c3ae68ba498540483d2c0bb
Xinhan Yang <xinhanyang@gatech.edu>
2024-05-03 12:06:10 -0700

    add torchmd_et distillation

--- matdeeplearn/models/torchmd_et.py
+++ matdeeplearn/models/torchmd_et.py
@@ -16,6 +16,7 @@ from matdeeplearn.models.base_model import BaseModel, conditional_grad
 from matdeeplearn.models.torchmd_output_modules import Scalar, EquivariantScalar
 from matdeeplearn.common.registry import registry
 from matdeeplearn.preprocessor.helpers import node_rep_one_hot
+from matdeeplearn.models.local_structure import DistanceNet
 @registry.register_model("torchmd_et")
 
 
@@ -56,6 +57,15 @@ class TorchMD_ET(BaseModel):
             higher values if they are using higher upper distance cutoffs and expect more
             than 32 neighbors per node/atom.
             (default: :obj:`32`)
+        use_distill (bool): Whether model is used for distillation or not. 
+            (default: :obj:`False`)
+        distill_layers (string, optional): The layer features used for distillation.
+        projection_head (bool): Set two linear layer for feature mapping. 
+            (default: :obj:`False`)
+        id_mapping (bool): Set identical mapping for feature mapping. 
+            (default: :obj:`False`)
+        force_linear_n2n (bool): Set one linear layer for feature mapping. 
+            (default: :obj:`False`)
     """
 
     def __init__(
@@ -80,6 +90,13 @@ class TorchMD_ET(BaseModel):
         pool="global_mean_pool",
         pool_order="late",
         aggr="add",
+        use_distill=False,
+        distill_layers = "",
+        projection_head=False,
+        id_mapping=False,
+        force_linear_n2n=False,
+        is_teacher=False,
+        mapping_layers=1,
         **kwargs
     ):
         super(TorchMD_ET, self).__init__(**kwargs)
@@ -114,6 +131,17 @@ class TorchMD_ET(BaseModel):
         self.pool_order = pool_order
         self.output_dim = output_dim
         cutoff_lower = 0
+
+        self.use_distill = use_distill
+        self.projection_head = projection_head
+        self.id_mapping = id_mapping
+        self.force_linear_n2n = force_linear_n2n
+
+        self.teacher_edge_dim = 1
+        self.teacher_node_dim = 1
+        self.teacher_vec_dim = 1
+        self.is_teacher = is_teacher
+        self.mapping_layers = mapping_layers
 
         act_class = act_class_mapping[activation]
 
@@ -152,6 +180,8 @@ class TorchMD_ET(BaseModel):
             ).jittable()
             self.attention_layers.append(layer)
 
+        self.distance_net = DistanceNet()
+
         self.out_norm = nn.LayerNorm(hidden_channels)
 
         self.num_post_layers = num_post_layers
@@ -165,6 +195,54 @@ class TorchMD_ET(BaseModel):
         self.post_lin_list.append(nn.Linear(post_hidden_channels, self.output_dim))
 
         self.reset_parameters()
+
+        # set up distillation layers
+        if use_distill:
+            distill_layers = [
+                code.lower() for code in distill_layers.split("_")
+            ]
+            distill_layers = [
+                (code[0], int(code[1:])) for code in distill_layers
+            ]
+            distill_layers = sorted(
+                distill_layers, key=lambda tup: (tup[1], tup[0])
+            )  # Sort primarly on layer number, then on m/u
+            assert all(
+                code[1] > 0 for code in distill_layers
+            ), "Distill layer number in torchmd net is less than 1"
+            assert all(
+                code[1] <= (num_layers) for code in distill_layers
+            ), "Distill layer number in torchmd net is large than num_layers"
+            assert all(
+                code[0] in ["a"] for code in distill_layers
+            ), "Distill layer type in Painn not in a(attention)"
+            self.distill_layers = distill_layers
+            self.num_distill_layers = len(self.distill_layers)
+    
+    def create_mapping(self, input_dim, output_dim, num_layers):
+        layers = [torch.nn.Linear(input_dim, output_dim)]
+        for _ in range(num_layers - 1):  # Subtract 1 because the first layer is already added
+            layers += [torch.nn.SiLU(), torch.nn.Linear(output_dim, output_dim)]
+        return torch.nn.Sequential(*layers)
+    
+    def setup_distillation(self):
+        model_feature_dim = self.extract_feature_dimensions()
+
+        if self.is_teacher or self.id_mapping:
+            self.n2n_mapping = torch.nn.Identity()
+            self.v2v_mapping = torch.nn.Identity()
+            self.e2n_mapping = torch.nn.Identity()
+            self.e2e_mapping = torch.nn.Identity()
+        else:
+            self.n2n_mapping = self.create_mapping(
+                model_feature_dim["node_dim"], self.teacher_node_dim, self.mapping_layers)
+            self.e2n_mapping = self.create_mapping(
+                model_feature_dim["node_dim"], self.teacher_edge_dim, self.mapping_layers)
+            self.e2e_mapping = self.create_mapping(
+                model_feature_dim["edge_dim"], self.teacher_edge_dim, self.mapping_layers)
+            self.v2v_mapping = self.create_mapping(
+                model_feature_dim["vec_dim"], self.teacher_vec_dim, self.mapping_layers)
+
 
     def reset_parameters(self):
         self.embedding.reset_parameters()
@@ -270,7 +348,122 @@ class TorchMD_ET(BaseModel):
     @property
     def target_attr(self):
         return "y"
+    
+    @conditional_grad(torch.enable_grad())
+    def extract_feature(self, data):
 
+        output = {}
+
+        #set distillation feature vectors
+        node_feature = []
+        edge_feature = []     
+        vec_feature = []
+        local_structure_feature = []   
+        distill_layers_iter = iter(self.distill_layers)
+        distill_layer = next(distill_layers_iter, (None, None))
+
+        x = self.embedding(data.z)
+
+        #edge_index, edge_weight, edge_vec = self.distance(data.pos, data.batch)
+        #assert (
+        #    edge_vec is not None
+        #), "Distance module did not return directional information"
+        if self.otf_edge_index == True:
+            #data.edge_index, edge_weight, data.edge_vec, cell_offsets, offset_distance, neighbors = self.generate_graph(data, self.cutoff_radius, self.n_neighbors)   
+            data.edge_index, data.edge_weight, data.edge_vec, _, _, _ = self.generate_graph(data, self.cutoff_radius, self.n_neighbors)  
+        data.edge_attr = self.distance_expansion(data.edge_weight) 
+                            
+        #mask = data.edge_index[0] != data.edge_index[1]        
+        #data.edge_vec[mask] = data.edge_vec[mask] / torch.norm(data.edge_vec[mask], dim=1).unsqueeze(1)
+        data.edge_vec = data.edge_vec / torch.norm(data.edge_vec, dim=1).unsqueeze(1)
+        
+        if self.otf_node_attr == True:
+            data.x = node_rep_one_hot(data.z).float()          
+        
+        if self.neighbor_embedding is not None:
+            x = self.neighbor_embedding(data.z, x, data.edge_index, data.edge_weight, data.edge_attr)
+
+        vec = torch.zeros(x.size(0), 3, x.size(1), device=x.device)
+
+        for i in range(len(self.attention_layers)):
+            attn = self.attention_layers[i]
+            dx, dvec = attn(x, vec, data.edge_index, data.edge_weight, data.edge_attr, data.edge_vec)
+            x = x + dx
+            vec = vec + dvec
+            if distill_layer == ("a", i+1):
+                local_structure = self.distance_net(x, data.edge_index)
+                local_structure_feature.append(local_structure.clone())
+                node_feature.append(x.clone())
+                vec_feature.append(vec.clone())
+                edge_feature.append(data.edge_attr.clone())
+                distill_layer = next(distill_layers_iter, (None, None))
+        x = self.out_norm(x)
+        
+        if self.prediction_level == "graph":
+            if self.pool_order == 'early':
+                x = getattr(torch_geometric.nn, self.pool)(x, data.batch)
+            for i in range(0, len(self.post_lin_list) - 1):
+                x = self.post_lin_list[i](x)
+                x = getattr(F, self.activation)(x)
+            x = self.post_lin_list[-1](x)
+            if self.pool_order == 'late':
+                x = getattr(torch_geometric.nn, self.pool)(x, data.batch)
+            #x = self.pool.pre_reduce(x, vec, data.z, data.pos, data.batch)
+            #x = self.pool.reduce(x, data.batch)
+        elif self.prediction_level == "node":
+            for i in range(0, len(self.post_lin_list) - 1):
+                x = self.post_lin_list[i](x)
+                x = getattr(F, self.activation)(x)
+            x = self.post_lin_list[-1](x) 
+
+        output["output"] =  x
+        assert distill_layer == (None, None)
+        #node_feat = torch.cat(node_feature, dim=-1)     
+        #edge_feat = torch.cat(edge_feature, dim=-1)
+        #vec_feat = torch.cat(vec_feature, dim=-1)
+        if self.is_teacher:
+            edge_to_node_feat = [scatter(feature, data.edge_index[0], dim=0, reduce='mean') for feature in edge_feature]
+            output["e2n_mapping"] = [self.e2n_mapping(feature.float()) for feature in edge_to_node_feat]
+        else:
+            output["e2n_mapping"] = [self.e2n_mapping(feature.float()) for feature in node_feature]
+        output["n2n_mapping"] = [self.n2n_mapping(feature.float()) for feature in node_feature]
+        output["e2e_mapping"] = [self.e2e_mapping(feature.float()) for feature in edge_feature]
+        vec_feat = [vec.reshape(vec.size(0), -1) for vec in vec_feature]
+        v2v = [self.v2v_mapping(feature.float()) for feature in vec_feat]
+        output["v2v_mapping"] = v2v
+        output["local_structure"] = local_structure_feature
+        
+        if self.gradient == True and x.requires_grad == True:         
+            volume = torch.einsum("zi,zi->z", data.cell[:, 0, :], torch.cross(data.cell[:, 1, :], data.cell[:, 2, :], dim=1)).unsqueeze(-1)                        
+            grad = torch.autograd.grad(
+                    x,
+                    [data.pos, data.displacement],
+                    grad_outputs=torch.ones_like(x),
+                    create_graph=self.training) 
+            forces = -1 * grad[0]
+            stress = grad[1]
+            stress = stress / volume.view(-1, 1, 1)         
+
+            output["pos_grad"] =  forces
+            output["cell_grad"] =  stress
+        else:
+            output["pos_grad"] =  None
+            output["cell_grad"] =  None  
+                  
+        return output       
+    
+    def extract_feature_dimensions(self):
+        num_distill_layer = len(self.distill_layers)
+        node_feature_dim = self.hidden_channels
+        edge_feature_dim =  self.num_rbf
+        vec_feature_dim = 3 * self.hidden_channels
+        return {"node_dim":node_feature_dim, "edge_dim":edge_feature_dim, "vec_dim":vec_feature_dim}
+
+    def set_teacher_dim(self, teacher_dim):
+        self.teacher_edge_dim = teacher_dim.get("edge_dim", 1)
+        self.teacher_node_dim = teacher_dim.get("node_dim",1)
+        self.teacher_vec_dim = teacher_dim.get("vec_dim",1)
+ 
 
 class EquivariantMultiHeadAttention(MessagePassing):
     def __init__(
