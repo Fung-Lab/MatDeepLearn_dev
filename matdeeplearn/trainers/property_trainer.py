fa98dd98759f7a05a641c8f8a214e27386d7ee67
Fan Shu <fanshu0102@gmail.com>
2024-08-25 21:45:30 -0500

    finetune equiformer -- fix predict

--- matdeeplearn/trainers/property_trainer.py
+++ matdeeplearn/trainers/property_trainer.py
@@ -320,12 +320,11 @@ class PropertyTrainer(BaseTrainer):
 
         if write_output == True:
             if labels == True:
+                # self.save_results(
+                #     np.column_stack((ids,virtual_pos, target, predict)), results_dir, f"{split}_predictions.csv", node_level
+                # )
                 self.save_results(
-                    np.column_stack((ids,virtual_pos, target, predict)), results_dir, f"{split}_predictions.csv", node_level
-                )
-                self.save_results(
-                    np.column_stack((ids, target, predict)), results_dir, f"{split}_predictions.csv",
-                    node_level
+                    np.column_stack((ids, target, predict)), results_dir, f"{split}_predictions.csv", node_level
                 )
             else:
                 self.save_results(
