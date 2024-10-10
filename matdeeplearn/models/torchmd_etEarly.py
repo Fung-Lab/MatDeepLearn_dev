8329901029ab414255e18a12aaf248d9632f5647
Ian Slagle <islagle3@gatech.edu>
2024-10-10 16:37:36 -0400

    Tabs and spaces

--- matdeeplearn/models/torchmd_etEarly.py
+++ matdeeplearn/models/torchmd_etEarly.py
@@ -196,19 +196,15 @@ class TorchMD_ET_Early(BaseModel):
         if self.neighbor_embedding is not None:
             x = self.neighbor_embedding(data.z, x, data.edge_index, data.edge_weight, data.edge_attr)
 
-	print(x.requires_grad)
-
+        print(x.requires_grad)
         vec = torch.zeros(x.size(0), 3, x.size(1), device=x.device)
 
         for attn in self.attention_layers:
             dx, dvec = attn(x, vec, data.edge_index, data.edge_weight, data.edge_attr, data.edge_vec)
             x = x + dx
             vec = vec + dvec
-
-	print(x.requires_grad)
-	    
+        print(x.requires_grad)
 	x = self.out_norm(x)
-
 	print(x.requires_grad)
         
         if self.prediction_level == "graph":
